2e20dbb9b51387a2e65bdddc6347b01e53444afa
Luc Van Christopher ANDRIANAVALONIRINA <lucvanchristopher.andrianavalonirina2023@campus-eni.fr>
2024-02-01 15:55:58 +0100

    Formulaire
    il reste le boutom enregistrer qui ne marche pas en tous cas ce qu'il montre mais ca s'enregistre sur la base de donnée

---
+++ ProjetCatalogueProduit/Controllers/CategorieController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetCatalogueProduit.Models;
+
+namespace ProjetCatalogueProduit.Controllers
+{
+    public class CategorieController : Controller
+    {
+        BD_CATALOGUE_PRODUITEntities db = new BD_CATALOGUE_PRODUITEntities();
+        // GET: Categorie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjoutCategorie()
+        {
+            try 
+            { 
+                ViewBag.listeCategorie = db.CAT_CATEGORIE.ToList();
+                return View(); 
+            } catch(Exception e) 
+            { 
+                return HttpNotFound(); 
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AjoutCategorie(CAT_CATEGORIE categorie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var db = new BD_CATALOGUE_PRODUITEntities())
+                    {
+                        db.CAT_CATEGORIE.Add(categorie);
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+                return View(categorie);
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
